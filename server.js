d5def3c9eba7c425e4c966a4152c7fcf5ba76a6f
ymnberkay <berkay.yaman@hotmail.com>
2023-03-24 19:24:34 +0300

    edited

--- server.js
+++ server.js
@@ -14,8 +14,16 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 const notesSchema = {
     title: String,
     content: String,
-    conten2: String
+    text: String,
+    text2: String,
+    text3: String,
+    text4: String,
+    text5: String,
+    text6: String,
+    text7: String,
+
 }
+
 
 const Note = mongoose.model("Note", notesSchema);
 
@@ -27,7 +35,14 @@ app.post("/", function(req, res) {
     let newNote = new Note({
         title: req.body.title,
         content: req.body.content,
-        content2: req.body.content2
+        text: req.body.text1,
+        text2: req.body.text2,
+        text3: req.body.text3,
+        text4: req.body.text4,
+        text5: req.body.text5,
+        text6: req.body.text6,
+        text7: req.body.text7,
+
     });
     newNote.save();
     res.redirect('/');
